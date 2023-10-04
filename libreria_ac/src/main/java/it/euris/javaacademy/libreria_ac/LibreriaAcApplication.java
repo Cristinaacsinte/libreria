6e1766a20b8e6f3b823d2adc6de97e353aab21c6
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-10-04 18:42:27 +0200

    Add file

---
+++ libreria_ac/src/main/java/it/euris/javaacademy/libreria_ac/LibreriaAcApplication.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.libreria_ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaAcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaAcApplication.class, args);
+	}
+
+}
---
+++ libreria_ac/src/test/java/it/euris/javaacademy/libreria_ac/LibreriaAcApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.libreria_ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
