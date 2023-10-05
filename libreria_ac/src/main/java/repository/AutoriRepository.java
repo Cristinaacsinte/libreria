7c9638ebe80d8fb8b2d23fde400149835f80bae5
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-10-05 08:30:34 +0200

    Add All Repository

---
+++ libreria_ac/src/main/java/repository/AutoriRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Autori;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoriRepository extends JpaRepository<Autori, Long> {
+}
---
+++ libreria_ac/src/main/java/repository/CasaEditriceRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.CasaEditrice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CasaEditriceRepository extends JpaRepository<CasaEditrice,Long> {
+}
