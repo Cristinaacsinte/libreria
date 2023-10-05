81f65ddfaa6772ae94259f78dc2bd827061f5e3b
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-10-05 09:04:49 +0200

    Add Autori Service Impl

--- libreria_ac/src/main/java/Service/impl/AutoriServiceImpl.java
+++ libreria_ac/src/main/java/Service/impl/AutoriServiceImpl.java
@@ -2,8 +2,11 @@ package Service.impl;
 
 import Service.AutoriService;
 import entity.Autori;
+import exeption.IdMustBeNullException;
+import exeption.IdMustNotBeNullException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.AutoriRepository;
 
 import java.util.List;
 
@@ -11,28 +14,36 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class AutoriServiceImpl implements AutoriService {
+    AutoriRepository autoriRepository;
     @Override
     public List<Autori> findAll() {
-        return null;
+        return autoriRepository.findAll();
     }
 
     @Override
     public Autori insert(Autori autori) {
-        return null;
+        if(autori.getId() != null){
+            throw new IdMustBeNullException();
+        }
+        return autoriRepository.save(autori);
     }
 
     @Override
     public Autori update(Autori autori) {
-        return null;
+        if(autori.getId() == null){
+            throw new IdMustNotBeNullException();
+        }
+        return autoriRepository.save(autori);
     }
 
     @Override
     public Boolean deleteById(Long idAutori) {
-        return null;
+        autoriRepository.deleteById(idAutori);
+        return autoriRepository.findById(idAutori).isEmpty();
     }
 
     @Override
     public Autori findById(Long idAutori) {
-        return null;
+        return autoriRepository.findById(idAutori).orElse(Autori.builder().build());
     }
 }
