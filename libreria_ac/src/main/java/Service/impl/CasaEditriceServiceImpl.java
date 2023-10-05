5860dd24f2249118ccdfced92e1763f41a1b4ce5
Cristina Acsinte <cristtina.acsinte@gmail.com>
2023-10-05 09:00:16 +0200

    Add Casa Editrice Service Impl

--- libreria_ac/src/main/java/Service/impl/CasaEditriceServiceImpl.java
+++ libreria_ac/src/main/java/Service/impl/CasaEditriceServiceImpl.java
@@ -2,32 +2,43 @@ package Service.impl;
 
 import Service.CasaEditriceService;
 import entity.CasaEditrice;
+import exeption.IdMustBeNullException;
+import exeption.IdMustNotBeNullException;
+import repository.CasaEditriceRepository;
 
 import java.util.List;
 
 public class CasaEditriceServiceImpl implements CasaEditriceService {
+    CasaEditriceRepository casaEditriceRepository;
     @Override
     public List<CasaEditrice> findAll() {
-        return null;
+        return casaEditriceRepository.findAll();
     }
 
     @Override
     public CasaEditrice insert(CasaEditrice casaEditrice) {
-        return null;
+        if(casaEditrice.getId() != null){
+            throw new IdMustBeNullException();
+        }
+        return casaEditriceRepository.save(casaEditrice);
     }
 
     @Override
     public CasaEditrice update(CasaEditrice casaEditrice) {
-        return null;
+        if(casaEditrice.getId() == null){
+            throw new IdMustNotBeNullException();
+        }
+        return casaEditriceRepository.save(casaEditrice);
     }
 
     @Override
     public Boolean deleteById(Long idCasaEditrice) {
-        return null;
+        casaEditriceRepository.deleteById(idCasaEditrice);
+        return casaEditriceRepository.findById(idCasaEditrice).isEmpty();
     }
 
     @Override
     public CasaEditrice findById(Long idCasaEditrice) {
-        return null;
+        return casaEditriceRepository.findById(idCasaEditrice).orElse(CasaEditrice.builder().build());
     }
 }
